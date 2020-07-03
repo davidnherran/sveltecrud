1b34698c8e432abd7c46e054f6367f48e13882ca
David Niño Herrán <45274593+davidnherran@users.noreply.github.com>
2020-07-02 21:28:56 -0500

    Agregando binds

--- src/App.svelte
+++ src/App.svelte
@@ -14,15 +14,22 @@
 <main>
 
   <form action="">
-    <input type="text" placeholder="Product name" id="product-name" />
+    <input bind:value={product.name} type="text" placeholder="Product name" id="product-name" />
     <textarea
+      bind:value={product.description}
       id="product-description"
       rows="3"
       placeholder="Product description" />
     <input
+      bind:value={product.imageURL}
       type="url"
       id="product-image-url"
       placeholder="https://github.com/davidnherran/sveltecrud" />
+    <select id="category" bind:value={product.category}>
+      <option value="laptops">Laptops</option>
+      <option value="peripherials">Peripherials</option>
+      <option value="servers">Servers</option>
+    </select>
     <button>Save product</button>
   </form>
 
